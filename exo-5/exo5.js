78d1684c4ea73bc6db5d121c7547f9c7e2b05cdd
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-03-27 13:58:36 +0100

    feat: add exo-5

---
+++ exo-5/exo5.js
@@ -0,0 +1,19 @@
+// you can write js here
+
+// console.log('exo-5');
+
+let input = "T’inquiète pas, les baleines ça mange que du kril..."
+let vowels = ["a", "à", "â", "ä", "e", "é", "è", "ê", "ë", "i", "î", "ï", "o", "u"]
+let resultArray = []
+
+for (i = 0; i < input.length; i++) {
+    for (j = 0; j < vowels.length; j++) {
+        if (input.charAt(i).toUpperCase() === vowels[j].toUpperCase()) {
+            resultArray.push(vowels[j].toUpperCase())
+        }
+    }
+}
+
+resultArray = resultArray.join("")
+
+console.log(resultArray)
