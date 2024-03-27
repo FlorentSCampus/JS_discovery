fa3bddca49c9f667456eef02552a49f58c827d3b
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-03-27 14:00:58 +0100

    feat: modified array content of vowels

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -3,7 +3,7 @@
 // console.log('exo-5');
 
 let input = "T’inquiète pas, les baleines ça mange que du kril..."
-let vowels = ["a", "à", "â", "ä", "e", "é", "è", "ê", "ë", "i", "î", "ï", "o", "u"]
+let vowels = ["a", "à", "â", "ä", "e", "é", "è", "ê", "ë", "i", "î", "ï", "o", "ô", "ö", "u", "û", "ü"]
 let resultArray = []
 
 for (i = 0; i < input.length; i++) {
